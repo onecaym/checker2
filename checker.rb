b5b354143985b51d9367323a632a1cdc32e35ccd
onecaym <osipov99@list.ru>
2020-07-01 20:19:29 +0400

    names of methods was changed

--- checker.rb
+++ checker.rb
@@ -7,31 +7,31 @@ class Checker
     @folder = folder
   end
 
-  def content_of_given_folder
+  def folder_items
     Dir.children(@folder)
   end
 
-  def file_reader(dir, file_name)
+  def file_content(dir, file_name)
     File.read("#{dir}#{file_name}")
   end
 
-  def sha1_encoder(dir, file_name)
-    Digest::SHA1.hexdigest file_reader(dir, file_name)
+  def encoded_content(dir, file_name)
+    Digest::SHA1.hexdigest file_content(dir, file_name)
   end
 
-  def hash_converter
-    content_of_given_folder.map do |file_name|
-      { file_name => sha1_encoder(@folder, file_name) }
+  def array_of_hashes
+    folder_items.map do |file_name|
+      { file_name => encoded_content(@folder, file_name) }
     end
   end
 
   def repetitive_value
-    uniq_value = hash_converter.map { |hash| hash.values.join }
+    uniq_value = array_of_hashes.map { |hash| hash.values.join }
     uniq_value.detect { |code| uniq_value.count(code) > 1 }
   end
 
   def check
-    hash_converter.map do |hash|
+    array_of_hashes.map do |hash|
       if repetitive_value == hash.values.first
         puts "#{hash.keys.first} - Использован несколько раз"
       else
