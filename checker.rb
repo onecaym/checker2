46b112b3fbd22c70ac0e1d42e9c40a7c62c58485
onecaym <osipov99@list.ru>
2020-08-06 19:31:16 +0400

    2 methods deleted

--- checker.rb
+++ checker.rb
@@ -6,24 +6,16 @@ class Checker
   def initialize(folder)
     @folder = folder
     @grouped_files = {}
-    check
-  end
-
-  def check
-   group_files
+    group_files
   end
 
   def duplicated_files
-    find_duplicated_files(grouped_files)
+    grouped_files.values.select { |files| files.count > 1 }
   end
 
   private
 
   attr_reader :folder, :grouped_files
-
-  def find_duplicated_files(hash)
-    hash.values.select { |files| files.count > 1 }
-  end
 
   def folder_items
     Dir.children(folder)
